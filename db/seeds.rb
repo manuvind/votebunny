250c38ab2d874487f0ead5786d66829fd42862b3
Anuvind Venu Menon <manuvind@gmail.com>
2014-05-05 13:56:14 -0700

    create postions, candidates, and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # create election
-e = Election.create!(:title => 'IEEE Director Elections')
+e = Election.create!(:title => 'IEEE Elections')
 
 # create admin
-u = User.new(:password => 'noelmon69', :is_admin => true)
+u = User.new(:password => 'manu1337', :is_admin => true)
 u.elections << e
 u.save!
 
@@ -20,23 +20,33 @@ passwords = ['yX679', 'ywer7', 'aax67', 'smd69', 'gnwer9', 'asf7s', '91xls',
           'pus11', 'uio99', 'asdf2', 'us934', 'sdfna', '09bcs', 'sdfj1',
           '0sdbn', 'sdf11', 'mane1', 'pqo24', 'ps7aq', 'usZfj', 'ifj4w',
           'ksi88']
+=end
+passwords = []
+(1..45).each do |u|
+  passwords.push((0...3).map { ('a'..'z').to_a[rand(26)] }.join + SecureRandom.random_number(100).to_s)
+end
 passwords.each do |pw|
   u = User.new(:password => pw, :is_admin => false)
   u.elections << e
   u.save!
 end
-=end
+
 
 # create positions and candidates
-=begin
+
 runners = {
-  'Indrel Director' => ['Jamshed', 'Harrison', 'Vikram', 'Marion'],
-  'Activities Director' => ['Ino', 'Leland', 'Alice'],
-  'TechOps Director' => ['Eugene', 'Felix'],
-  'Website Director' => ['Yuval', 'Kevin', 'Steve', 'Tom', 'Felix'],
-  'IO Director' => ['Lauren', 'Jonathan', 'Flora', 'Nate', 'Marion', 'Tabitha'],
-  'HOPE Director' => ['Jeff', 'Andrew'],
-  'Prodev Director' => ['Skyler']
+  'President' => ['Vikram', 'Skyler'],
+  'IVP' => ['Gokul', 'Ingrid', 'Skyler'],
+  'EVP' => ['Skyler', 'Michael', 'Vikram'],
+  'Treasurer' => ['Tom', 'Michael', 'Jamshed'],
+  'Secretary' => ['Gokul'],
+  'Indrel Director' => ['Jamshed', 'Luca'],
+  'Activities Director' => ['Gokul', 'Lauren', 'Lori'],
+  'TechOps Director' => ['Gautham', 'Roy'],
+  'Website Director' => ['Alex', 'Nate'],
+  'IO Director' => ['Jack', 'Ingrid', 'Lauren'],
+  'HOPE Director' => ['Ronald', 'Andrew', 'James', 'Chitanya', 'Victor', 'Max', 'Sudip'],
+  'Prodev Director' => ['Vikram N', 'Colin', 'Michael']
 }
 
 runners.each do |position, candidates|
@@ -48,4 +58,4 @@ runners.each do |position, candidates|
     c.save!
   end
 end
-=end
+
